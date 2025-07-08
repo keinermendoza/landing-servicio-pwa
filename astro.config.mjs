ce25b76a4855fe16da98628773e64281ce01d212
Keiner Mendoza <keinermendoza@gmail.com>
2025-07-08 00:47:36 -0400

    feat: layout ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,37 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+// https://flowbite.com/docs/getting-started/astro/
+export default defineConfig({
+  experimental: {
+      fonts: [
+          {
+              provider: fontProviders.google(),
+              name: "Plus Jakarta Sans",
+              cssVariable: "--font-jakarta",
+              weights: [400,800] ,
+          },
+          {
+              provider: fontProviders.google(),
+              name: "Inter",
+              cssVariable: "--font-inter",
+              weights: [400,700] ,
+          }
+      ]
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@assets': path.resolve('./src/assets'),
+        '@pages': path.resolve('./src/pages'),
+
+      }
+    }
+  },
+ 
+});
